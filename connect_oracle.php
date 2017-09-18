45fff81d8f9042a549a40834c62fead53fbf187f
Tiago Murakami <trmurakami@gmail.com>
2017-09-18 16:33:52 -0300

    Correção

--- connect_oracle.php
+++ connect_oracle.php
@@ -12,125 +12,127 @@ function processaFixes ($marc,$id){
 
 /* Processa os fixes */
 
-switch ($marc["record"]["BAS"]["a"][0]) {
-    case "Catalogação Rápida":
-        echo "Não indexar";
-        break;
-	case "Assinatura Combinada":
-        echo "Não indexar";
-        break;		
-    case 01:        
-		if ($marc["record"]["945"]["b"][0] == "PARTITURA"){
+if (isset($marc["record"]["BAS"])) {
 
-			// $index = "partituras";
-			// $body = fixes($marc);
+	switch ($marc["record"]["BAS"]["a"][0]) {
+		case "Catalogação Rápida":
+			echo "Não indexar";
+			break;
+		case "Assinatura Combinada":
+			echo "Não indexar";
+			break;		
+		case 01:        
+			if ($marc["record"]["945"]["b"][0] == "PARTITURA"){
 
-			// if (isset($marc["record"]["260"])) {
-			// 	if (isset($marc["record"]["260"]["c"])){
-			// 		$excluir_caracteres = array("[","]","c");
-			// 		$only_numbers = str_replace($excluir_caracteres, "", $marc["record"]["260"]["c"][0]);
-			// 		$body["doc"]["datePublished"] = $only_numbers;
-			// 	} else {
-			// 		$body["doc"]["datePublished"] = "N/D";
-			// 	}	
-					
-			// }
-			// $body["doc"]["base"][] = "Livros";
-            // $response = elasticsearch::elastic_update($id,"partitura",$body);
-            // //print_r($response);
+				// $index = "partituras";
+				// $body = fixes($marc);
 
-		} elseif ($marc["record"]["945"]["b"][0] == "TRABALHO DE CONCLUSAO DE CURSO - TCC") {
-			//$index = "bdta_homologacao";
-			//$body = fixes($marc);
-			//$body["doc"]["base"][] = "Trabalhos acadêmicos";
-			//$body["doc"]["sysno"] = $id;
-			//if (isset($marc["record"]["260"])) {
-			//	if (isset($marc["record"]["260"]["c"])){
-			//		$excluir_caracteres = array("[","]","c");
-			//		$only_numbers = str_replace($excluir_caracteres, "", $marc["record"]["260"]["c"][0]);
-			//		$body["doc"]["datePublished"] = $only_numbers;
-			//	} else {
-			//		$body["doc"]["datePublished"] = "N/D";
-			//	}	
-					
-			//}				
-            //$response = elasticsearch::elastic_update($id,$type,$body);
-            //print_r($response);
-		} elseif ($marc["record"]["945"]["b"][0] == "TRABALHO DE ESPECIALIZACAO - TCE") {
-			//$index = "bdta_homologacao";
-			//$body = fixes($marc);
-			//$body["doc"]["base"][] = "Trabalhos acadêmicos";
-			//$body["doc"]["sysno"] = $id;
-			//if (isset($marc["record"]["260"])) {
-			//	if (isset($marc["record"]["260"]["c"])){
-			//		$excluir_caracteres = array("[","]","c");
-			//		$only_numbers = str_replace($excluir_caracteres, "", $marc["record"]["260"]["c"][0]);
-			//		$body["doc"]["datePublished"] = $only_numbers;
-			//	} else {
-			//		$body["doc"]["datePublished"] = "N/D";
-			//	}	
-					
-			//}				
-            //$response = elasticsearch::elastic_update($id,$type,$body);
-            //print_r($response);
+				// if (isset($marc["record"]["260"])) {
+				// 	if (isset($marc["record"]["260"]["c"])){
+				// 		$excluir_caracteres = array("[","]","c");
+				// 		$only_numbers = str_replace($excluir_caracteres, "", $marc["record"]["260"]["c"][0]);
+				// 		$body["doc"]["datePublished"] = $only_numbers;
+				// 	} else {
+				// 		$body["doc"]["datePublished"] = "N/D";
+				// 	}	
+						
+				// }
+				// $body["doc"]["base"][] = "Livros";
+				// $response = elasticsearch::elastic_update($id,"partitura",$body);
+				// //print_r($response);
 
-		} elseif ($marc["record"]["945"]["b"][0] == "E-BOOK") {
-			// $index = "ebooks";
-			// $body = fixes($marc);
+			} elseif ($marc["record"]["945"]["b"][0] == "TRABALHO DE CONCLUSAO DE CURSO - TCC") {
+				//$index = "bdta_homologacao";
+				//$body = fixes($marc);
+				//$body["doc"]["base"][] = "Trabalhos acadêmicos";
+				//$body["doc"]["sysno"] = $id;
+				//if (isset($marc["record"]["260"])) {
+				//	if (isset($marc["record"]["260"]["c"])){
+				//		$excluir_caracteres = array("[","]","c");
+				//		$only_numbers = str_replace($excluir_caracteres, "", $marc["record"]["260"]["c"][0]);
+				//		$body["doc"]["datePublished"] = $only_numbers;
+				//	} else {
+				//		$body["doc"]["datePublished"] = "N/D";
+				//	}	
+						
+				//}				
+				//$response = elasticsearch::elastic_update($id,$type,$body);
+				//print_r($response);
+			} elseif ($marc["record"]["945"]["b"][0] == "TRABALHO DE ESPECIALIZACAO - TCE") {
+				//$index = "bdta_homologacao";
+				//$body = fixes($marc);
+				//$body["doc"]["base"][] = "Trabalhos acadêmicos";
+				//$body["doc"]["sysno"] = $id;
+				//if (isset($marc["record"]["260"])) {
+				//	if (isset($marc["record"]["260"]["c"])){
+				//		$excluir_caracteres = array("[","]","c");
+				//		$only_numbers = str_replace($excluir_caracteres, "", $marc["record"]["260"]["c"][0]);
+				//		$body["doc"]["datePublished"] = $only_numbers;
+				//	} else {
+				//		$body["doc"]["datePublished"] = "N/D";
+				//	}	
+						
+				//}				
+				//$response = elasticsearch::elastic_update($id,$type,$body);
+				//print_r($response);
 
-			// if (isset($marc["record"]["260"])) {
-			// 	if (isset($marc["record"]["260"]["c"])){
-			// 		$excluir_caracteres = array("[","]","c");
-			// 		$only_numbers = str_replace($excluir_caracteres, "", $marc["record"]["260"]["c"][0]);
-			// 		$body["doc"]["datePublished"] = $only_numbers;
-			// 	} else {
-			// 		$body["doc"]["datePublished"] = "N/D";
-			// 	}	
-					
-			// }
-			// $body["doc"]["base"][] = "E-Books";
-            // $response = elasticsearch::elastic_update($id,$type,$body);
-            // //print_r($response);	
+			} elseif ($marc["record"]["945"]["b"][0] == "E-BOOK") {
+				// $index = "ebooks";
+				// $body = fixes($marc);
 
-		} else {
+				// if (isset($marc["record"]["260"])) {
+				// 	if (isset($marc["record"]["260"]["c"])){
+				// 		$excluir_caracteres = array("[","]","c");
+				// 		$only_numbers = str_replace($excluir_caracteres, "", $marc["record"]["260"]["c"][0]);
+				// 		$body["doc"]["datePublished"] = $only_numbers;
+				// 	} else {
+				// 		$body["doc"]["datePublished"] = "N/D";
+				// 	}	
+						
+				// }
+				// $body["doc"]["base"][] = "E-Books";
+				// $response = elasticsearch::elastic_update($id,$type,$body);
+				// //print_r($response);	
 
-			$body = fixes($marc);
-			if (isset($marc["record"]["260"])) {
-			  	if (isset($marc["record"]["260"]["c"])){
-			  		$excluir_caracteres = array("[","]","c");
-			  		$only_numbers = str_replace($excluir_caracteres, "", $marc["record"]["260"]["c"][0]);
-			  		$body["doc"]["datePublished"] = $only_numbers;
-			  	} else {
-			 		$body["doc"]["datePublished"] = "N/D";
-				}					
+			} else {
+
+				$body = fixes($marc);
+				if (isset($marc["record"]["260"])) {
+					if (isset($marc["record"]["260"]["c"])){
+						$excluir_caracteres = array("[","]","c");
+						$only_numbers = str_replace($excluir_caracteres, "", $marc["record"]["260"]["c"][0]);
+						$body["doc"]["datePublished"] = $only_numbers;
+					} else {
+						$body["doc"]["datePublished"] = "N/D";
+					}					
+				}
+				$body["doc"]["base"][] = "Livros";
+				$response = elasticsearch::elastic_update($id,$type,$body,"opac");
+				print_r($response);
+				
 			}
-			$body["doc"]["base"][] = "Livros";
-			$response = elasticsearch::elastic_update($id,$type,$body,"opac");
+			break;
+		case 02:
+			echo "Não indexar";
+			break;
+		case 03:
+			$body = fixes($marc);
+			$body["doc"]["base"][] = "Teses e dissertações";
+			$body["doc"]["sysno"] = $id;
+			$response = elasticsearch::elastic_update($id,$type,$body,$index);
 			print_r($response);
-			
-		}
-        break;
-    case 02:
-        echo "Não indexar";
-        break;
-    case 03:
-		$body = fixes($marc);
-		$body["doc"]["base"][] = "Teses e dissertações";
-		$body["doc"]["sysno"] = $id;
-        $response = elasticsearch::elastic_update($id,$type,$body,$index);
-        print_r($response);
-        break;
-    case 04:
-		$body = fixes($marc);
-		$body["doc"]["base"][] = "Produção científica";
-		$body["doc"]["sysno"] = $id;
-        $response = elasticsearch::elastic_update($id,$type,$body,$index);
-        print_r($response);
-        break;
-    default:
-        break;
+			break;
+		case 04:
+			$body = fixes($marc);
+			$body["doc"]["base"][] = "Produção científica";
+			$body["doc"]["sysno"] = $id;
+			$response = elasticsearch::elastic_update($id,$type,$body,$index);
+			print_r($response);
+			break;
+		default:
+			break;
+	}
 }
-
 }
 
 function  oracle_sysno($sysno) {
