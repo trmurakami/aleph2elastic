9701527d4e53fae5e4a6b02964e0598622d07f7b
Tiago Murakami <trmurakami@gmail.com>
2018-05-14 14:58:14 -0300

    Adicionado assinaturas combinadas

--- bulk_import_alephseq.php
+++ bulk_import_alephseq.php
@@ -23,14 +23,17 @@ while( $line = fgets(STDIN) ) {
 
 	/* Processa os fixes */
 
+
 	switch ($marc["record"]["BAS"]["a"][0]) {
 		case "Catalogação Rápida":
 			echo "Não indexar";
 			break;
+		case "Assinatura Combinada":
+			echo "Não indexar";
+			break;		
 		case 01:        
 			if ($marc["record"]["945"]["b"][0] == "PARTITURA"){
 
-				$index = "partituras";
 				$body = fixes($marc);
 				if (isset($marc["record"]["260"])) {
 					if (isset($marc["record"]["260"]["c"])){
@@ -39,15 +42,14 @@ while( $line = fgets(STDIN) ) {
 						$body["doc"]["datePublished"] = $only_numbers;
 					} else {
 						$body["doc"]["datePublished"] = "N/D";
-					}	
-					 
+					}					
 				}
-				$body["doc"]["base"][] = "Livros";
-				$response = elasticsearch::elastic_update($id,"partitura",$body);
+				$body["doc"]["base"][] = "Partituras";
+				$response = elasticsearch::elastic_update($id,"partitura",$body,"partituras");
+				print_r($response);				
 
 			} elseif ($marc["record"]["945"]["b"][0] == "TRABALHO DE CONCLUSAO DE CURSO - TCC") {
 
-				$index = "bdta_homologacao";
 				$body = fixes($marc);
 				$body["doc"]["base"][] = "Trabalhos acadêmicos";
 				$body["doc"]["sysno"] = $id;
@@ -59,13 +61,13 @@ while( $line = fgets(STDIN) ) {
 					} else {
 						$body["doc"]["datePublished"] = "N/D";
 					}	
-					 
+						
 				}				
-				$response = elasticsearch::elastic_update($id,$type,$body);
+				$response = elasticsearch::elastic_update($id,$type,$body,"bdta_homologacao");
+				print_r($response);
 
 			} elseif ($marc["record"]["945"]["b"][0] == "TRABALHO DE ESPECIALIZACAO - TCE") {
 
-				$index = "bdta_homologacao";
 				$body = fixes($marc);
 				$body["doc"]["base"][] = "Trabalhos acadêmicos";
 				$body["doc"]["sysno"] = $id;
@@ -76,13 +78,31 @@ while( $line = fgets(STDIN) ) {
 						$body["doc"]["datePublished"] = $only_numbers;
 					} else {
 						$body["doc"]["datePublished"] = "N/D";
-					}					 
+					}	
+						
 				}				
-				$response = elasticsearch::elastic_update($id,$type,$body);
+				$response = elasticsearch::elastic_update($id,$type,$body,"bdta_homologacao");
+				print_r($response);
 
 			} elseif ($marc["record"]["945"]["b"][0] == "E-BOOK") {
 
-				$index = "ebooks";
+				$body = fixes($marc);
+
+				if (isset($marc["record"]["260"])) {
+					if (isset($marc["record"]["260"]["c"])){
+				 		$excluir_caracteres = array("[","]","c");
+				 		$only_numbers = str_replace($excluir_caracteres, "", $marc["record"]["260"]["c"][0]);
+				 		$body["doc"]["datePublished"] = $only_numbers;
+				 	} else {
+				 		$body["doc"]["datePublished"] = "N/D";
+				 	}	
+				}
+				$body["doc"]["base"][] = "E-Books";
+				$response = elasticsearch::elastic_update($id,$type,$body,"ebooks");
+				print_r($response);	
+
+			} else {
+
 				$body = fixes($marc);
 				if (isset($marc["record"]["260"])) {
 					if (isset($marc["record"]["260"]["c"])){
@@ -91,15 +111,13 @@ while( $line = fgets(STDIN) ) {
 						$body["doc"]["datePublished"] = $only_numbers;
 					} else {
 						$body["doc"]["datePublished"] = "N/D";
-					}						
+					}					
 				}
-				$body["doc"]["base"][] = "E-Books";
-				$response = elasticsearch::elastic_update($id,$type,$body);
-
-			} else {
-
+				$body["doc"]["base"][] = "Livros";
+				$response = elasticsearch::elastic_update($id,$type,$body,"opac");
+				print_r($response);
+				
 			}
-
 			break;
 		case 02:
 			echo "Não indexar";
@@ -109,19 +127,20 @@ while( $line = fgets(STDIN) ) {
 			$body["doc"]["base"][] = "Teses e dissertações";
 			$body["doc"]["sysno"] = $id;
 			$response = elasticsearch::elastic_update($id,$type,$body);
+			print_r($response);
 			break;
 		case 04:
 			$body = fixes($marc);
 			$body["doc"]["base"][] = "Produção científica";
 			$body["doc"]["sysno"] = $id;
 			$response = elasticsearch::elastic_update($id,$type,$body);
+			print_r($response);
 			break;
 		default:
-			$body = fixes($marc);
-			$body["doc"]["base"][] = $marc["record"]["BAS"]["a"][0];
-			$body["doc"]["sysno"] = $id;
-			$response = elasticsearch::elastic_update($id,$type,$body);		
-	}	
+			break;
+	}
+
+	
 	echo "$sysno \n";
             
   $marc = [];
